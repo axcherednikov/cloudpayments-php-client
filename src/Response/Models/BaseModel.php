b7de99ef0dae762732014de22cd9b3d8637f267a
Alexander <agel63@mail.ru>
2026-06-08 14:17:41 +0300

    Fix dynamic transaction response properties

--- src/Response/Models/BaseModel.php
+++ src/Response/Models/BaseModel.php
@@ -9,13 +9,43 @@ use stdClass;
  */
 class BaseModel
 {
+    /** @var array<string, mixed> */
+    private array $additionalProperties = [];
+
     public function fill(stdClass $fillData): void
     {
         $props = get_object_vars($fillData);
+        $knownProperties = $this->getKnownProperties();
 
         foreach ($props as $key => $value) {
             $lowerKey = lcfirst($key);
-            $this->{$lowerKey} = $value;
+
+            if (isset($knownProperties[$lowerKey])) {
+                $this->{$lowerKey} = $value;
+
+                continue;
+            }
+
+            $this->additionalProperties[$lowerKey] = $value;
         }
+    }
+
+    /**
+     * @return array<string, true>
+     */
+    private function getKnownProperties(): array
+    {
+        $properties = get_class_vars(static::class);
+        unset($properties['additionalProperties']);
+
+        return array_fill_keys(array_keys($properties), true);
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function getAdditionalProperties(): array
+    {
+        return $this->additionalProperties;
     }
 }
